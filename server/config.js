b65650720cf424498098dbf85bd4fd6202298c2d
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-04 17:48:29 -0700

    Set up basic server and dummy client

---
+++ server/config.js
@@ -0,0 +1,14 @@
+// MODULES ==========================================
+var express = require('express');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+// CONFIGURATION ====================================
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/../client'));
+
+module.exports = app;
