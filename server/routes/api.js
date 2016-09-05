aafdb18182653ec9731d1ed70968dabb78219708
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-04 23:24:51 -0700

    Implement basic auth to check for API key

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
+var basicAuth = require('basic-auth');
 var Task = require('../db');
+
+var auth = function(req, res, next) {
+  var unauthorized = function(res) {
+    res.set('WWW_Authenticate', 'Basic realm=Authorization Required');
+    return res.sendStatus(401);
+  };
+
+  var user = basicAuth(req);
+
+  if (!user || !user.name) {
+    return unauthorized(res);
+  }
+
+  return (user.name === 'my_api_key') ? next() : unauthorized(res);
+};
+
+router.use(auth);
 
 router.route('/annotation')
   .get(function(req, res) {
     Task.find(function(err, tasks) {
-      (err) ? res.send(err) : res.json(tasks);
+      (err) ? res.send(err) : res.status(200).json(tasks);
     });
   })
 
