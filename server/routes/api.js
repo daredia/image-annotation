9d173ed167f8413ab9bfa7193fd41755c0911143
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-04 22:46:24 -0700

    Tweak indentation

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,16 +4,18 @@ var router = express.Router();
 var Task = require('../db');
 
 router.route('/annotation')
-.get(function(req, res) {
-  Task.find(function(err, tasks) {
-    (err) ? res.send(err) : res.json(tasks);
-  });
-})
-.post(function(req, res) {
-  var task = new Task(req.body);
-  task.save(function(err, task) {
-    (err) ? res.send(err) : res.status(200).json(task);
-  });
-});
+  .get(function(req, res) {
+    Task.find(function(err, tasks) {
+      (err) ? res.send(err) : res.json(tasks);
+    });
+  })
+
+  .post(function(req, res) {
+    var task = new Task(req.body);
+    task.save(function(err, task) {
+      (err) ? res.send(err) : res.status(200).json(task);
+    });
+  }
+);
 
 module.exports = router;
