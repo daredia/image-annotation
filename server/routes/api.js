c55a5a483a8f1d3a906e5929d30bc700d6007057
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-04 18:34:32 -0700

    Create basic post handler for annotation route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 router.get('/annotation', function(req, res) {
   res.send('Welcome to the annotation endpoint');
+});
+
+router.post('/annotation', function(req, res) {
+  res.status(200).json(req.body);
 });
 
 module.exports = router;
