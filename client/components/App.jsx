34a85c00f93c36f0213dc2375f571359f30220b6
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-09 16:59:34 -0400

    Fetch tasks from server when component mounts

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
+import axios from 'axios';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      image: "http://blog.teamtreehouse.com/wp-content/uploads/2015/05/InternetSlowdown_Day.gif"
+    };
+  }
 
+  componentDidMount() {
+    axios.get('http://localhost:4567/v1/task/annotation', {
+      auth: {
+        username: 'my_api_key'
+      }
+    })
+    .then((res) => console.log(res));
+  }
+
+  createAnnotator() {
     var editor = new BBoxAnnotator({
-      url: "http://i.imgur.com/trsL3fU.jpg",
+      url: this.state.image,
       onchange: function(annotation) {
         $("#annotation_data").val(JSON.stringify(annotation));
       },
       input_method: 'select',
       labels: ['girl', 'IE man']
     });
-
   }
 
   render() {
+    this.createAnnotator();
     return (
       <div>
         <div>
