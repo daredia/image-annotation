e532e2140e621867c5b1bb29a0ad25bcce90e778
Shehzad Daredia <shehzad.daredia@gmail.com>
2016-09-12 20:58:23 -0400

    Fix path references to static assets

--- server/config.js
+++ server/config.js
@@ -13,6 +13,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/../node_modules'));
 
 // ROUTES ===========================================
 app.use('/', routes);
